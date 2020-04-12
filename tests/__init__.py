6119de80a131b31cfb797a024bf9d9c082b03d6a
Sebastian Bechtold <mr.bechtold@gmail.com>
2020-04-12 07:50:42 +0200

    Re-enabled all unit tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,7 +4,7 @@
 
 from .test_existence_of_endpoints import *
 
-'''
+
 from .test_post_entities_entity_attrs import *
 from .test_patch_entities_entity_attrs import *
 
@@ -18,4 +18,4 @@ from .test_post import *
 from .test_put import *
 from .test_temporal_entities import *
 from .test_xdatatogo import *
-'''
+
